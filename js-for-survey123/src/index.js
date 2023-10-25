9195f284b6b0e3b0e9bcac89e138242d9e8a693c
Marcelo Rigoto <89153170+marcelorigoto1@users.noreply.github.com>
2023-10-25 13:58:31 -0300

    Update index.js
    A more specific get away using these functions for a offline survey.

--- js-for-survey123/src/index.js
+++ js-for-survey123/src/index.js
@@ -1,55 +1,210 @@
-import { console, globals, fetchSync } from "./survey123/lib.js";
-import txt from "./test.txt";
-import main from "./main.js";
-import proj4 from "proj4";
+import booleanIntersects from "@turf/boolean-intersects";
+import nearestPoint from "@turf/nearest-point";
+import { point } from "@turf/helpers";
+import { featureCollection } from "@turf/helpers";
+import pointsWithinPolygon from "@turf/points-within-polygon";
+import { lineString } from "@turf/helpers";
 
-export function importText() {
-  console.log(`\`${globals.libraryName}.importText\` method invoked`);
-  return txt;
+//------------------------------------------Infos Gerais-------------------------------------------//
+//
+//  V.1.0 - Funções Para o Funcionamento do Survey123 com o Node para consultas Offline.
+//  V.1.1 - Códigos foram refatorados para melhorar a recursividade e entendimente.
+//        - Em relação a performace o calculo do objeto mais próximo melhorou e não causa
+//          mais travamentos na máquina.
+//
+//  Em primeiro momento, é possível utilizar a função Feature to JSON do ArcPRO.
+//  Para o funcionamento das funções é necessário retirar as linhas de começo dos JSONs até a 
+//  [feature], ainda é necessário realizar essa etapa para facilitar a edição dos dados
+//  por outros usuários.
+//
+//
+//  A biblioteca utilizada para as funções foi a TURF.js (https://turfjs.org/)
+//  as funções podem ser consultadas no link para documentação.
+//
+//
+//  A funções com <export> são exportadas para o Survey, caso seja só necessário dentro de outra função
+//  é somente necessário declarar como function
+//
+//-------------------------------------------------------------------------------------------------//
+//-----------------------------------------Início do código----------------------------------------//
+//
+//  Transformam as linhas desenhados dos surveys, sendo necessário retirar o campo de altura 
+//  e os colchetes para a função de FeatureCollection conseguir agrupar os pontos como uma reta
+//
+//-------------------------------------------------------------------------------------------------//
+
+function transformaJsonLinha(survey123Linha) {
+  let tracado = (survey123Linha);
+  let tiraAltitude = JSON.stringify(tracado).replace(/,0]/g, "]");
+  let tiraFinal = ((tiraAltitude).replace(']]]', ']]'));
+  let tiraComeco = ((tiraFinal).replace('[[[', '[['));
+  let linhaString = JSON.parse(tiraComeco);
+  return linhaString;
 }
 
-export function importJson() {
-  console.log(`\`${globals.libraryName}.importJson\` method invoked`);
-  return main().object.string;
+//-------------------------------------------------------------------------------------------------//
+//
+//  Transformam os poligonos desenhados dos surveys, sendo necessário retirar o campo de altura 
+//  e os colchetes para a função de FeatureCollection conseguir agrupar os pontos 
+//  como um poligono para o intersect
+//
+//-------------------------------------------------------------------------------------------------//
+
+function transformaJsonPoligono(survey123Poligono) {
+  let poligonoString = featureCollection(survey123Poligono);
+  let tiraAltitude = JSON.stringify(poligonoString).replace(/,0]/g, "]");
+  let tiraComeco = (tiraAltitude).replace('{"type":"FeatureCollection","features":{"spatialReference":{"wkid":4326},"rings":',
+                                          '[{"type":"Feature","id":1,"geometry":{"type":"Polygon","coordinates":');
+  let tiraFinal = ((tiraComeco).replace(']]]}}', ']]]}}]'));
+  let poligono = JSON.parse(tiraFinal);
+  return poligono;
 }
 
-export function HelloWorld() {
-  return JSON.stringify({ Hola: "Mundo" });
-}
+//-------------------------------------------------------------------------------------------------//
+//
+//  Funções para puxarem os JSONs das áreas de proteção ambiental 
+//  e intersectarem com as linhas inputados pelos usuários
+//  atráves da função de booleanIntersects da biblioteca TURF.js
+//
+//-------------------------------------------------------------------------------------------------//
 
-export function fetchTest(url, where, token) {
-  try{
-  var queryString = url + "/query?f=json&outfields=*";
-  if (where) queryString += "&where=" + where;
-  console.log(queryString);
-  return JSON.stringify(fetchSync(queryString).json());
-  } catch (e){
-    return JSON.stringify({error: e.message})
-  }
-}
-
-export function reprojectPoint(x, y) {
+function checkLineIntersection(survey123Line, filePath) {
   try {
-    if (isNaN(x) || isNaN(y)) {
-      return "Please provide numbers for x or y";
-    }
-
-    if (!isFinite(x) || !isFinite(y)) {
-      return "Please provide finite numbers for x or y";
-    }
-
-    //https://spatialreference.org/ref/epsg/indian-1960-utm-zone-48n/proj4/
-    var projectTo =
-      "+proj=utm +zone=48 +a=6377276.345 +b=6356075.41314024 +units=m +no_defs";
-    return proj4(projectTo, [x, y]);
+    let jsonPolygon = filePath;
+    let polygonCollection = featureCollection(jsonPolygon);
+    let tracado = transformaJsonLinha(survey123Line);
+    let linestring = lineString(tracado.paths, { name: 'linha' });
+    let intersection = booleanIntersects(linestring, polygonCollection);
+    return intersection ? 'Verdadeiro' : 'Falso';
   } catch (e) {
-    return "";
+    return JSON.stringify({ error: e.message });
   }
 }
 
-export function getConsole() {
-  return console.getOutput({ sizeLimit: 4096 });
+export function intersectLinhaIndigena(survey123Line) {
+  return checkLineIntersection(survey123Line, require('./TI_SC.json'));
 }
+
+export function intersectLinhaApp(survey123Line) {
+  return checkLineIntersection(survey123Line, require('./APP_SC.json'));
+}
+
+export function intersectLinhaUnidade(survey123Line) {
+  return checkLineIntersection(survey123Line, require('./UCs_SC.json'));
+}
+
+
+//-------------------------------------------------------------------------------------------------//
+//
+//  Funções para puxarem os JSONs das áreas de proteção ambiental 
+//  e intersectarem com os poligonos inputados pelos usuários
+//  atráves da função de booleanIntersects da biblioteca TURF.js
+//
+//-------------------------------------------------------------------------------------------------//
+
+function checkIntersection(survey123Poligono, filePath) {
+  try {
+    let jsonPolygon = filePath;
+    let polygonCollection = featureCollection(jsonPolygon);
+    let poligono = transformaJsonPoligono(survey123Poligono);
+    let poligonoTransform = featureCollection(poligono);
+    let intersection = booleanIntersects(poligonoTransform, polygonCollection);
+    return intersection ? 'Verdadeiro' : 'Falso';
+  } catch (e) {
+    return JSON.stringify({ error: e.message });
+  }
+}
+
+export function intersectIndigena(survey123Poligono) {
+  return checkIntersection(survey123Poligono, require('./TI_SC.json'));
+}
+
+export function intersectApp(survey123Poligono) {
+  return checkIntersection(survey123Poligono, require('./APP_SC.json'));
+}
+
+export function intersectUnidade(survey123Poligono) {
+  return checkIntersection(survey123Poligono, require('./UCs_SC.json'));
+}
+
+//-------------------------------------------------------------------------------------------------//
+//
+//  Funções para puxarem os JSONs das áreas de proteção ambiental 
+//  e compararem com os pontos atraves da função de WithinPolygon 
+//  da biblioteca TURF.js inputados pelos usuários
+//
+//-------------------------------------------------------------------------------------------------//
+
+function checkWithinPolygon(input, filePath) {
+  try {
+    var target = filePath;
+    var targetFeature = featureCollection(target);
+    var lat = JSON.stringify(input.x);
+    var long = JSON.stringify(input.y);
+    var inputPonto = point([lat, long], { name: 'Ponto de Input' });
+    var dentro = pointsWithinPolygon(inputPonto, targetFeature);
+    var final = JSON.stringify(dentro.features);
+    return final.length < 3 ? 'Falso' : 'Verdadeiro';
+  } catch (e) {
+    return JSON.stringify({ error: e.message });
+  }
+}
+
+export function withinApp(input) {
+  return checkWithinPolygon(input, require('./APP_SC.json'));
+}
+
+export function withinIndigena(input) {
+  return checkWithinPolygon(input, require('./TI_SC.json'));
+}
+
+export function withinUnidade(input) {
+  return checkWithinPolygon(input, require('./UCs_SC.json'));
+}
+
+//-------------------------------------------------------------------------------------------------//
+//
+//  Funções para puxarem os JSONs dos trechos
+//  e pegarem a propriedade trecho dos pontos mais perto do ponto inputado pelo usuário
+//  através da função nearestPoint da biblioteca TURF.js
+//
+//-------------------------------------------------------------------------------------------------//
+
+function getNearestProperty(acesso, property) {
+  try {
+      var arquivo = require('./TLT_SC.json');
+      var pontos_arquivo = featureCollection(arquivo);
+      var lat = JSON.stringify(acesso.x);
+      var long = JSON.stringify(acesso.y);
+      var locationA = point([lat, long], { name: 'Ponto de Input' });
+      var local = nearestPoint(locationA, pontos_arquivo);
+      var name = local.properties[property];
+      return name;
+  } catch (e) {
+      return JSON.stringify({ error: e.message });
+  }
+}
+
+export function getNearest(acesso) {
+  return getNearestProperty(acesso, 'LOCAL_INSTALACAO');
+}
+
+export function getTorre(acesso) {
+  return getNearestProperty(acesso, 'NOME_ESTRUT');
+}
+
+export function getLinha(acesso) {
+  return getNearestProperty(acesso, 'LT_SAP');
+}
+
+export function getCentro(acesso) {
+  return getNearestProperty(acesso, 'NOME_CENTRO_MAN');
+}
+
+export function getArea(acesso) {
+  return getNearestProperty(acesso, 'AREA_OPE');
+}
+
 
 export { dev } from "./survey123/lib.js";
 
